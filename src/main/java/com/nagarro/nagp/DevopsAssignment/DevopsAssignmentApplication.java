45bb16c8102ddc872d6c8c2645a0450d075f4153
vinodgodara <vinod.godara@nagarro.com>
2019-08-25 16:14:57 +0530

    Initialisation of the repository.

---
+++ src/main/java/com/nagarro/nagp/DevopsAssignment/DevopsAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.nagarro.nagp.DevopsAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nagarro/nagp/DevopsAssignment/DevopsAssignmentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nagarro.nagp.DevopsAssignment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DevopsAssignmentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
